eb8087d4f5174aa4226ba39df4854bbbded09d0e
Shao Li <shaohuan.li@gmail.com>
2012-08-30 00:44:16 +0800

    Added a simple register function

---
+++ register.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<form action="api/register.php" method="post">
+email<input type="text" name="email"/>
+password<input type="password" name="password"/>
+<input type="submit" name="submit"/>
+</form>
+</body>
+</html>
