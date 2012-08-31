7c73639ee216ef51b2d8c41a647d83f53748743f
Shao Li <shaohuan.li@gmail.com>
2012-08-31 10:55:00 +0800

    Rewrote The API

--- api/constants.php
+++ api/constants.php
@@ -3,5 +3,5 @@ define("DB_SERVER","localhost");
 define("DB_NAME","asplan");
 define("DB_USER","root");
 define("DB_PASS","giveMePass");
-define("SERVER_URL","122.248.240.151");
+define("SERVER_URL","http://localhost/hotel");
 ?>
