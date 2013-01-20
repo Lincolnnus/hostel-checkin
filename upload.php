00acf18a28982a2d5693ca6073eda655a4752636
lincolnnus <shaohuan.li@gmail.com>
2013-01-21 00:45:10 +0800

    Added the Upload Module

--- upload.php
+++ upload.php
@@ -1,15 +1,31 @@
 <html>
 <head>
+<script src="js/jquery.min.js"></script>
+<script src="js/cookie.js"></script>
+<script>
+$(document).ready(function() {
+                  if(checkCookie("uid")==0){
+                  window.location="index.php";
+                  }else{
+                  	var uid=getCookie("uid");
+                  	$("#uid").val(uid);
+                  	var token=getCookie("token");
+                  	$("#token").val(token);
+                  	var hid=getCookie("hid");
+                  	$("#hid").val(hid);
+                  }
+                  });
+</script>
 </head>
 <body>
-<form action="api/upload.php" method="post"
-enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file" /> 
-<input type="hidden" name="uid" value="1"/>
-<br />
-<input type="submit" name="submit" value="Upload" />
-</form>
-
+  <form  action="api/admin.php" method="POST" enctype="multipart/form-data" data-ajax="false">
+              <input type="file" name="file" id="file" /> 
+              <input type="hidden" name="token" id="token" /> 
+              <input type="hidden" name="uid" id="uid"/>
+              <input type="hidden" name="hid" id="hid"/>
+              <input type="hidden" name="action" id="action" value="uploadLogo"/>
+              <br />
+              <input type="submit" name="submit" value="Upload" />
+ </form>
 </body>
 </html>
